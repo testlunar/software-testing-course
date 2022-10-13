d007f55596c3471b568d827e74f80c96532a38d3
testlunar <diana.umudova@gmail.com>
2022-10-13 23:15:07 +0300

    тест №3

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 public class MainClassTest {
     MainClass mc = new MainClass();
 
@@ -13,5 +15,13 @@ public class MainClassTest {
     @Test
     void testGetClassNumber(){
         Assertions.assertTrue(mc.getClassNumber()>45,"Number is <= 45");
+    }
+
+    @Test
+    void testGetClassString(){
+
+        Assertions.assertTrue(mc.getClassString().toLowerCase().contains("hello"),
+                "Text does not contain hello or Hello");
+
     }
 }
